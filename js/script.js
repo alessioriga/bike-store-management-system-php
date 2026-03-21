8010891a801be0ec990dcaaddf688d98f2b1dffd
Alessio Rigamonti <alessioriga93@hotmail.it>
2026-03-21 12:09:09 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
+//Script for editing items
+function showEditForm(id) {
+    
+    const row = document.querySelector(`form input[name='item_id'][value='${id}']`).closest('tr');
+
+  
+    document.getElementById('editForm').style.display = 'block';
+    document.getElementById('item_id').value = id;
+    document.getElementById('item_name_edit').value = row.children[2].innerText;        
+    document.getElementById('description_edit').value = row.children[3].innerText;    
+    document.getElementById('colour_edit').value = row.children[4].innerText;         
+    document.getElementById('price_edit').value = row.children[5].innerText.replace('£',''); 
+    document.getElementById('quantity_edit').value = row.children[6].innerText;       
+    document.getElementById('current_image').value = row.querySelector('input[name="current_image"]').value;
+
+    window.scrollTo({ top: document.getElementById('editForm').offsetTop, behavior: 'smooth' });
+}
+
+
+function hideEditForm() {
+    document.getElementById('editForm').style.display = 'none';
+}
